a682b49a4fccfd594d7a4d3b017d85d42d66fb3c
Edward Chan <chankaward@gmail.com>
2014-11-27 15:38:43 +0800

    update nginx init ubuntu script link

--- nginx_init.sh
+++ nginx_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Download nginx-init-ubuntu
-wget https://gist.githubusercontent.com/chankaward/26f219de40ec42b81ea4/raw/841ab28b04595e8a079d5d9f304542128e90d1fa/nginx
+wget https://raw.githubusercontent.com/chankaward/ubuntu-handy-build/master/nginx
 
 # Replace nginx
 sudo mv ./nginx /etc/init.d/nginx
