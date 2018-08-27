6acf547fd12197af0ad51ce85a0cce6bad1421b8
David <lexuszhi1990@gmail.com>
2018-08-27 15:32:34 +0800

    Update tmux_latest_install.sh
    add -y for install

--- tmux_latest_install.sh
+++ tmux_latest_install.sh
@@ -2,7 +2,7 @@
 
 # http://bogdanvlviv.com/posts/tmux/how-to-install-the-latest-tmux-on-ubuntu-16_04.html
 sudo apt-get -y remove tmux
-sudo apt-get install libevent-dev libncurses-dev automake
+sudo apt-get install -y libevent-dev libncurses-dev automake
 
 rm -fr ~/tmux
 git clone https://github.com/tmux/tmux.git ~/tmux
