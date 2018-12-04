70dad4b204e92ca19f8654726b2937e90cc26b7e
David Fu <lexuszhi1990@gmail.com>
2018-12-04 14:43:42 +0800

    update install docker shell

--- docker_nvidia_cuda_install_ubuntu_1604.sh
+++ docker_nvidia_cuda_install_ubuntu_1604.sh
@@ -15,6 +15,8 @@ add-apt-repository \
 apt-get update
 apt-get -y -qq --no-install-recommends install docker-ce=18.06.1~ce~3-0~ubuntu
 
+sudo usermod -aG docker $USER
+
 # If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
 # docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
 apt-get purge -y nvidia-docker
@@ -42,6 +44,7 @@ sudo tee /etc/docker/daemon.json <<EOF
 }
 EOF
 pkill -SIGHUP dockerd
+systemctl restart docker
 
 # Test nvidia-smi with the latest official CUDA image
 docker run --rm nvidia/cuda nvidia-smi
